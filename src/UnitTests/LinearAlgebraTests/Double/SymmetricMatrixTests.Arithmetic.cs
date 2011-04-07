501a7a630e19ccb995937015b82b86901efdbee7
Alexander Karatarakis <alex@karatarakis.com>
2011-04-07 13:40:02 +0300

    Fixed IndexTester test to be consistent with mkl. Was erroneously following another indexing scheme.

--- src/UnitTests/LinearAlgebraTests/Double/SymmetricMatrixTests.Arithmetic.cs
+++ src/UnitTests/LinearAlgebraTests/Double/SymmetricMatrixTests.Arithmetic.cs
@@ -57,7 +57,7 @@ namespace MathNet.Numerics.UnitTests.LinearAlgebraTests.Double
                              { "Symmetric3x3", new[,] { { 1.0, 2.0, 3.0 }, { 2.0, 2.0, 0.0 }, { 3.0, 0.0, 3.0 } } },
                              { "NonSymmetric3x3", new[,] { { -1.1, -2.2, -3.3 }, { 0.0, 1.1, 2.2 }, { -4.4, 5.5, 6.6 } } },
                              { "NonSymmetric4x4", new[,] { { -1.1, -2.2, -3.3, -4.4 }, { 0.0, 1.1, 2.2, 3.3 }, { 1.0, 2.1, 6.2, 4.3 }, { -4.4, 5.5, 6.6, -7.7 } } },
-                             { "IndexTester", new double[,] { { 0, 1, 2, 3 }, { 1, 4, 5, 6 }, { 2, 5, 7, 8 }, { 3, 6, 8, 9 } } }
+                             { "IndexTester4x4", new double[,] { { 0, 1, 3, 6 }, { 1, 2, 4, 7 }, { 3, 4, 5, 8 }, { 6, 7, 8, 9 } } }
                          };
 
             TestMatrices = new Dictionary<string, Matrix>();

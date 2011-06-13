336030be3cc714e29d10115dab41b75519f18448
Alexander Karatarakis <alex@karatarakis.com>
2011-06-13 21:04:03 +0300

    [TriangularLowerMatrix] Add override and overloads for DoPointwiseMultiply()
    Signed-off-by: Alexander Karatarakis <alex@karatarakis.com>

--- src/Numerics/LinearAlgebra/Double/TriangularLowerMatrix.cs
+++ src/Numerics/LinearAlgebra/Double/TriangularLowerMatrix.cs
@@ -679,5 +679,59 @@ namespace MathNet.Numerics.LinearAlgebra.Double
                 }
             }
         }
+
+        /// <summary>
+        /// Pointwise multiplies this matrix with another matrix and stores the result into the result matrix.
+        /// </summary>
+        /// <param name="other">
+        /// The matrix to pointwise multiply with this one.
+        /// </param>
+        /// <param name="result">
+        /// The matrix to store the result of the pointwise multiplication.
+        /// </param>
+        protected override void DoPointwiseMultiply(Matrix<double> other, Matrix<double> result)
+        {
+            for (var row = 0; row < RowCount; row++)
+            {
+                for (var column = 0; column <= row; column++)
+                {
+                    result.AtLower(row, column, AtLower(row, column) * other.AtLower(row, column));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Pointwise multiplies this matrix with another matrix and stores the result into the result matrix.
+        /// </summary>
+        /// <param name="other">
+        /// The matrix to pointwise multiply with this one.
+        /// </param>
+        /// <param name="result">
+        /// The matrix to store the result of the pointwise multiplication.
+        /// </param>
+        protected void DoPointwiseMultiply(TriangularUpperMatrix other, Matrix<double> result)
+        {
+            for (var row = 0; row < RowCount; row++)
+            {
+                result.AtDiagonal(row, AtDiagonal(row) * other.AtDiagonal(row));
+            }
+        }
+
+        /// <summary>
+        /// Pointwise multiplies this matrix with another matrix and stores the result into the result matrix.
+        /// </summary>
+        /// <param name="other">
+        /// The matrix to pointwise multiply with this one.
+        /// </param>
+        /// <param name="result">
+        /// The matrix to store the result of the pointwise multiplication.
+        /// </param>
+        protected void DoPointwiseMultiply(DiagonalMatrix other, Matrix<double> result)
+        {
+            for (var row = 0; row < RowCount; row++)
+            {
+                result.AtDiagonal(row, AtDiagonal(row) * other.AtDiagonal(row));
+            }
+        }
     }
 }
